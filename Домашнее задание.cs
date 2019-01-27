ef1630158ada868eca48c6e28323e63ad8beb198
Azat <46015824+serikovskii@users.noreply.github.com>
2019-01-27 19:36:31 +0600

    Create Домашнее задание.cs

---
+++ Домашнее задание.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Материалы изучены \nПервое консольное приложение");
+
+        }
+    }
+
+}
