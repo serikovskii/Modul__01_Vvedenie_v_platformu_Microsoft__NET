f974fb3582e482d0230893d8306a24e17a3b6737
Azat <46015824+serikovskii@users.noreply.github.com>
2019-01-27 19:36:36 +0600

    Create Практическая работа.cs

---
+++ Практическая работа.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Mukushev Azat");
+            Console.WriteLine("Введите 2 числа:");
+            int numOne = Int32.Parse(Console.ReadLine());
+            int numTwo = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(numOne + numTwo);
+
+        }
+    }
+
+}
