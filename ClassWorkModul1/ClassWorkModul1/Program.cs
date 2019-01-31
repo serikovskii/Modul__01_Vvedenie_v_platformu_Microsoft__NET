0885db6fefa7e0988763a6a19b087f3f2e40c2c3
Azat <46015824+serikovskii@users.noreply.github.com>
2019-01-31 12:52:11 +0600

    перезалив

---
+++ ClassWorkModul1/ClassWorkModul1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWorkModul1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mukushev Azat");
+            Console.WriteLine("Введите 2 числа:");
+            int numOne = Int32.Parse(Console.ReadLine());
+            int numTwo = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(numOne + numTwo);
+        }
+    }
+}
