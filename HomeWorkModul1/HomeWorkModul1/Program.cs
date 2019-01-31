8f96953b0d603b15b0aab9928882d5b7472b270a
Azat <46015824+serikovskii@users.noreply.github.com>
2019-01-31 12:55:54 +0600

    перезалив дз

---
+++ HomeWorkModul1/HomeWorkModul1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkModul1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Материалы изучены \nПервое консольное приложение");
+            Console.ReadLine();
+        }
+    }
+}
